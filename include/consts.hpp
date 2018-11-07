d091ce83872f3ea820b185597c893a3bd31d951b
Alain <alain.riou14000@yahoo.com>
2018-11-07 08:36:43 +0100

    article

--- include/consts.hpp
+++ include/consts.hpp
@@ -2,8 +2,8 @@
 
 #include <SFML/Graphics.hpp>
 
-const int WINDOW_WIDTH = 2200;
-const int WINDOW_HEIGHT = 1600;
+const int WINDOW_WIDTH = 1200;
+const int WINDOW_HEIGHT = 800;
 const sf::Color WINDOW_BACKGROUND_COLOR = sf::Color::Black;
 
 const int BACTERIA_RADIUS = 10;
